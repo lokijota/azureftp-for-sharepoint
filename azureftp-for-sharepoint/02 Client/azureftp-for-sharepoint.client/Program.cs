820283e2723619fd87953b7ab5e38649b79bf5d2
João Pedro Martins <jota@create.pt>
2013-04-03 11:39:42 +0100

    Versão inicial da estrutura de projectos e dependências de clientOM de SharePoint2013 (x64).

---
+++ azureftp-for-sharepoint/02 Client/azureftp-for-sharepoint.client/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace azureftp_for_sharepoint.client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("open a connection to your sharepoint and then send in commands");
+        }
+    }
+}
