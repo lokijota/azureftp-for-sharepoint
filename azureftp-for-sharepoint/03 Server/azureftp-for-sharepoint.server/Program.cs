dd571301b8f5921b3964bbe86482c29d503b642a
João Pedro Martins <jota@create.pt>
2013-04-03 12:31:26 +0100

    Added installer to service. Added WIX as dependencies. Added links.txt file

--- azureftp-for-sharepoint/03 Server/azureftp-for-sharepoint.server/Program.cs
+++ azureftp-for-sharepoint/03 Server/azureftp-for-sharepoint.server/Program.cs
@@ -16,7 +16,7 @@ namespace azureftp_for_sharepoint.server
             ServiceBase[] ServicesToRun;
             ServicesToRun = new ServiceBase[] 
             { 
-                new Service1() 
+                new Server() 
             };
             ServiceBase.Run(ServicesToRun);
         }
---
+++ azureftp-for-sharepoint/03 Server/azureftp-for-sharepoint.server/Server.cs
@@ -0,0 +1,30 @@
+﻿namespace azureftp_for_sharepoint.server
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.ServiceProcess;
+    using System.Text;
+    using System.IO;
+
+    public partial class Server : ServiceBase
+    {
+        public Server()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            File.AppendAllText("log.server.log", string.Format("{0} Server Started", DateTime.Now.ToShortTimeString()));
+        }
+
+        protected override void OnStop()
+        {
+            File.AppendAllText("log.server.log", string.Format("{0} Server Stopped", DateTime.Now.ToShortTimeString()));
+        }
+    }
+}
