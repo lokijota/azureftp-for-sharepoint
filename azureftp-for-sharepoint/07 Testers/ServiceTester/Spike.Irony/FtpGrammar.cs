cddc4f060ba5a8b9ddddda4779194cc08268b2ed
João Pedro Martins <jota@create.pt>
2013-04-05 03:09:19 +0100

    Working on spike for Irony language parser. Already created base structure, but it's not working with commands that have parameters.

---
+++ azureftp-for-sharepoint/07 Testers/ServiceTester/Spike.Irony/FtpGrammar.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Irony;
+using Irony.Interpreter;
+using Irony.Parsing;
+
+namespace Spike.Irony
+{
+    [Language("AZFTP", "1.0", "Azure Ftp For SharePoint Language")]
+    public class FtpGrammar : Grammar
+    {
+
+        /// <summary>
+        /// Class constructor and grmamar definition
+        /// </summary>
+        public FtpGrammar() : base(false)
+        {
+            var open = ToTerm("open");
+            var cd = ToTerm("cd");
+            var dir = ToTerm("dir");
+            var close = ToTerm("close");
+            var get = ToTerm("get");
+            var backfolder = ToTerm("..");
+            var rootfolder = ToTerm(@"\");
+
+            var program = new NonTerminal("program");
+            var commandList = new NonTerminal("commandList");
+            var command = new NonTerminal("command");
+            var openCommand = new NonTerminal("open");
+            var cdCommand = new NonTerminal("cd");
+            var dirCommand = new NonTerminal("dir");
+            var closeCommand = new NonTerminal("close");
+            var getCommand = new NonTerminal("get");
+
+            var string_literal = new StringLiteral("string", ",", StringOptions.AllowsDoubledQuote);
+            //new RegexBasedTerminal
+
+            var url = new DsvLiteral("url", TypeCode.String);
+            var username = new DsvLiteral("username", TypeCode.String);
+            var password = new DsvLiteral("password", TypeCode.String);
+            var foldername = new DsvLiteral("foldername", TypeCode.String);
+            var filename = new DsvLiteral("filename", TypeCode.String);
+
+            this.Root = program;
+
+            program.Rule = commandList;
+            commandList.Rule = MakePlusRule(commandList, null, command);
+            command.Rule = openCommand | cdCommand | dirCommand | closeCommand | getCommand;
+
+            openCommand.Rule = open + string_literal + string_literal + string_literal + NewLine;
+            cdCommand.Rule = cd + rootfolder + NewLine | cd + backfolder + NewLine | cd + string_literal + NewLine;
+            dirCommand.Rule = dir + NewLine;
+            closeCommand.Rule = close + NewLine;
+            getCommand.Rule = get + filename + NewLine;
+
+            //this.MarkPunctuation(
+            //this.RegisterPunctuation("set", "camera", "size", ":", "by", "pixels", ".", "position", ",", "move");
+        }
+    }
+}
--- azureftp-for-sharepoint/07 Testers/ServiceTester/Spike.Irony/Program.cs
+++ azureftp-for-sharepoint/07 Testers/ServiceTester/Spike.Irony/Program.cs
@@ -2,17 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Irony; 
-using Irony.Parsing; // notajota: not recognized inside the namespace definition?!
+using Irony;
+using Irony.Parsing;
 using Irony.Interpreter;
-
+using Irony.Ast;
+ 
 namespace Spike.Irony
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("tbd");
+            Console.Title = "Ftp Irony Console Sample";
+            Console.WriteLine("Ftp Irony Console Sample.");
+            Console.WriteLine("");
+            Grammar grammar = new FtpGrammar();
+            var language = new LanguageData(grammar);
+            var runtime = new LanguageRuntime(language);
+            var commandLine = new CommandLine(runtime);
+            commandLine.Prompt = "AZFTP>";
+            commandLine.Run();
         }
     }
 }
